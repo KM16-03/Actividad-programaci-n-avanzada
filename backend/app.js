d4855d160e3ff12a3c4a645af9ea8ecf894bd8ad
KM16-03 <kevin.menendez21@gmail.com>
2026-03-31 00:02:21 -0600

    cambio semana6

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,14 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const isProduction = process.env.NODE_ENV === "production";
+
 var app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: isProduction
+    ? "https://habits-tracker-frontend-vite.onrender.com"
+    : "http://localhost:5173",
   credentials: true
 }));
 
